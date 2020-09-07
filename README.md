c8a23bc230178b42fee0c44396cff1dd826c8e73
Carlos E Hernández R <carlos.hernandez@correo.unimet.edu.ve>
2020-09-07 18:25:43 -0430

    update readme

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 The purpose of this project is to demonstrate my ability to collect, work with, and clean a data set. 
 
-##The goal 
+Skills: #Data Manipulation, #Regular Expression (REGEX), R Programming and Data Cleaning 
+
+## The Goal
 Prepare tidy data that can be used for later analysis.
 
-## The data set
+## The Data Set
 The data was collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
 
 http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
@@ -13,4 +15,8 @@ http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartpho
 Here are the data for the project:
 
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## How the script works
+
+
 
--- run_analysis.R
+++ run_analysis.R
@@ -5,124 +5,139 @@
 #'5 From the data set in step 4, creates a second, independent tidy data set with the average 
 #'of each variable for each activity and each subject.
 
-library(dplyr)
-library(data.table)
-
-#root directory
-rootpath <- "./UCI HAR Dataset"
-
-activityLabelsPath <- paste0(rootpath,"/activity_labels.txt")
-featuresPath <- paste0(rootpath,"/features.txt")
-
-####Features
-featuresRaw <- read.fwf(featuresPath, widths = c(3,1,35), sep = " ")
-features <- select(featuresRaw, V1,V4)
-names(features) <- c("FeatureIndex","FeatureName")
-
-analize <- function(type){
+#' this fuction run the script and write the file output
+#' @param rootpath the path of 'UCI HAR Dataset' folder, by default './UCI HAR Dataset'
+run <- function(rootpath = "./UCI HAR Dataset"){
   
-  #test directories
-  folderPath <- paste0(rootpath,paste0("/", type))
-  subjectsPath <- paste0(folderPath, paste0("/subject_", type, ".txt"))
+  #install.packages("dplyr")
+  library(dplyr)
   
-  xpath <- paste0(folderPath, paste0("/X_", type, ".txt"))
-  ypath <- paste0(folderPath, paste0("/y_", type, ".txt"))
-  
-  con <- file(xpath)
-  xraw <- readLines(con)
-  close(con)
-  
-  con <- file(ypath)
-  yraw <- readLines(con)
-  close(con)
-  
-  activityLabelsRaw <- read.fwf(activityLabelsPath, widths = c(1,1,20))
-
-
-  ####Subject
-  subjectsRaw <- read.fwf(subjectsPath, widths = c(2), sep = " ")
-
-  subjects <- subjectsRaw
-  names(subjects) <- c("subject")
-  
-  ####Xtest
-  
-  parseX <- function(x){
-    row <- strsplit(x, split = " ")
-    row <- lapply(row, strtrim, width = 14)
-    row <- lapply(row, as.numeric)
-    row <- na.omit(row[1][[1]])
-    return(row)
-  }
-  
-  onlyMeanAndSd <- function(x){
+  #' This function analize the train or test data set and parse them to data frames
+  #' @param type the folder to read can be "test" or "train" 
+  analize <- function(type){
     
-    output <- c()
+    # 'activity_labels.txt'file path
+    activityLabelsPath <- paste0(rootpath,"/activity_labels.txt")
+    # 'features.txt' file path
+    featuresPath <- paste0(rootpath,"/features.txt")
     
-    for (i in 1:length(x)){
-      
-      condition <- length(grep("[Mm]ean+|[Ss]d+", paste(features$FeatureName[i]), value = TRUE))>0
-      if (condition){
-        aux <-  c(list(variable = features$FeatureName[i], value= x[i]))
-        output <- rbind(output, aux,deparse.level = 0)
-      }
-      
+    # 'test' or 'train' data paths
+    folderPath <- paste0(rootpath,paste0("/", type))
+    subjectsPath <- paste0(folderPath, paste0("/subject_", type, ".txt"))
+    xpath <- paste0(folderPath, paste0("/X_", type, ".txt"))
+    ypath <- paste0(folderPath, paste0("/y_", type, ".txt"))
+    
+    # reading files and put in a variable
+    featuresRaw <- read.fwf(featuresPath, widths = c(3,1,35), sep = " ")
+    features <- select(featuresRaw, V1,V4)
+    names(features) <- c("FeatureIndex","FeatureName")
+    
+    con <- file(xpath)
+    xraw <- readLines(con)
+    close(con)
+    
+    con <- file(ypath)
+    yraw <- readLines(con)
+    close(con)
+    
+    activityLabelsRaw <- read.fwf(activityLabelsPath, widths = c(1,1,20))
+    
+    subjectsRaw <- read.fwf(subjectsPath, widths = c(2), sep = " ")
+    
+    subjects <- subjectsRaw
+    names(subjects) <- c("subject")
+    
+    #' this function parse the data form 'X_test.txt' file by row
+    #' @param x a item of xraw 
+    parseX <- function(x){
+      row <- strsplit(x, split = " ")
+      row <- lapply(row, strtrim, width = 14)
+      row <- lapply(row, as.numeric)
+      row <- na.omit(row[1][[1]])
+      return(row)
     }
     
-    return(output)
+    xdata <- lapply(xraw, parseX)
+    
+    #' this function filter the variables with mean or sd in its names 
+    #' @param x a item of xdata 
+    onlyMeanAndSd <- function(x){
+      
+      output <- c()
+      
+      for (i in 1:length(x)){
+        # select only that contains mean or sd in its name
+        condition <- length(grep("[Mm]ean+|[Ss]d+", paste(features$FeatureName[i]), value = TRUE))>0
+        if (condition){
+          aux <-  c(list(variable = features$FeatureName[i], value= x[i]))
+          output <- rbind(output, aux,deparse.level = 0)
+        }
+        
+      }
+      
+      return(output)
+    }
+    
+    xdata <- lapply(xdata, onlyMeanAndSd)
+    
+    # parse yraw to integer and set the colnames as 'ActivityId'
+    ydata <- data.frame(as.integer(yraw))
+    names(ydata) <- c("ActivityId")
+    
+    # select the row of activityLabelsRaw ad set names 'ActivityId' and 'ActivityName'
+    activityLabels <- select(activityLabelsRaw, V1,V3)
+    names(activityLabels) <- c("ActivityId", "ActivityName")
+    
+    # join ytest and activityLabels by 'ActivityId'
+    ydataByLabel <- inner_join(ydata, activityLabels, by="ActivityId")
+    
+    # bind Subject column in dataset
+    dataset <- ydataByLabel
+    dataset$Subject = subjects$subject
+    
+    # bind Measurement column in dataset
+    dataset$Measurements = xdata
+    
+    return(dataset)
   }
   
-  xdata <- lapply(xraw, parseX)
-  xdata <- lapply(xdata, onlyMeanAndSd)
+  # analize 'test' and 'train' data
+  print("Analizing test and train data...")
+  test <- analize("test");train <- analize("train")
   
-  ####ytest
-  ydata <- data.frame(as.integer(yraw))
-  names(ydata) <- c("ActivityId")
+  print("Cleannig dataset...")
+  # bind 'test' and 'train' 
+  dataset <- rbind(test,train)
   
-  ####activity labels
-  activityLabels <- select(activityLabelsRaw, V1,V3)
-  names(activityLabels) <- c("ActivityId", "ActivityName")
+  # remove 'ActivityId' column 
+  tidydata <- select(dataset, ActivityName, Subject)
   
-  ####joining ytest and activityLabels
-  ydataByLabel <- inner_join(ydata, activityLabels, by="ActivityId")
-  
-  ####join an unique dataset
-  dataset <- ydataByLabel
-  dataset$Subject = subjects$subject
-  
-  dataset$Measurements = xdata
-  
-  return(dataset)
-}
-
-##read test and train data
-test <- analize("test");train <- analize("train")
-####merge 
-dataset <- rbind(test,train)
-####cleanning
-tidydata <- select(dataset, ActivityName, Subject)
-
-######Step 4#######
-for (i in 1:53) {
-  aux <- c()
-  variable <- dataset$Measurements[1][[1]][i,]$variable
-  for (j in 1:dim(dataset)[1]) {
-    value <-dataset$Measurements[j][[1]][i,]$value
-    aux <- append(aux, value)
+  # create a column for each measurement variable 
+  for (i in 1:53) {
+    aux <- c()
+    variable <- dataset$Measurements[1][[1]][i,]$variable
+    for (j in 1:dim(dataset)[1]) {
+      value <-dataset$Measurements[j][[1]][i,]$value
+      aux <- append(aux, value)
+    }
+    
+    tidydata[variable] = aux 
   }
   
-  tidydata[variable] = aux 
+  #data set with the average of each variable for each activity and each subject
+  tidydata <- tidydata %>% group_by(Subject, ActivityName) %>% summarise_all(list(mean))
+  
+  print("Writing file...")
+  # Write in file
+  write.table(tidydata, file = "./tidy_dataset.txt", append = FALSE, quote = TRUE, sep = " ",
+              eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+              col.names = TRUE)
+  
+  print("File created: './tidy_dataset.txt'")
+  print("Script finished.")
 }
 
-#####grouping for a new dataset
-tidydata <- tidydata %>% group_by(Subject, ActivityName) %>% summarise_all(list(mean))
-
-#####Write on a file 
-write.table(tidydata, file = "./tidy_dataset.txt", append = FALSE, quote = TRUE, sep = " ",
-            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-            col.names = TRUE); print("Data writed at './tidy_dataset.txt'")
-
-###################
+run()
 
 
 
