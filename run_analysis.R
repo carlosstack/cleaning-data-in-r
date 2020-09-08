d0111b260a88d96c4a959ff1212b757815884ad3
Carlos E Hernández R <carlos.hernandez@correo.unimet.edu.ve>
2020-09-07 23:57:05 -0430

    ready for submit

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,5 @@
-#'1 Merges the training and the test sets to create one data set.
-#'2 Extracts only the measurements on the mean and standard deviation for each measurement.
-#'3 Uses descriptive activity names to name the activities in the data set
-#'4 Appropriately labels the data set with descriptive variable names.
-#'5 From the data set in step 4, creates a second, independent tidy data set with the average 
-#'of each variable for each activity and each subject.
 
-#' this fuction run the script and write the file output
+#' This prepare tidy data that can be used for later analysis.
 #' @param rootpath the path of 'UCI HAR Dataset' folder, by default './UCI HAR Dataset'
 run_analysis <- function(rootpath = "./UCI HAR Dataset"){
   
@@ -67,7 +61,7 @@ run_analysis <- function(rootpath = "./UCI HAR Dataset"){
       
       for (i in 1:length(x)){
         # select only that contains mean or sd in its name
-        condition <- length(grep("[Mm]ean+|[Ss]d+", paste(features$FeatureName[i]), value = TRUE))>0
+        condition <- length(grep("([Mm]ean+)|([Ss]td+)", paste(features$FeatureName[i]), value = TRUE))>0
         if (condition){
           aux <-  c(list(variable = features$FeatureName[i], value= x[i]))
           output <- rbind(output, aux,deparse.level = 0)
@@ -101,7 +95,7 @@ run_analysis <- function(rootpath = "./UCI HAR Dataset"){
     return(dataset)
   }
   
-  # analize 'test' and 'train' data
+  # analyze 'test' and 'train' data
   print("Analizing test and train data...")
   test <- analyze("test");train <- analyze("train")
   
